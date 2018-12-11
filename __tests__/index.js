463bdc2b2275033a38509263059308742785c091
Logan Murphy <lmurphy@learnvest.com>
2018-12-11 12:48:39 -0500

    add failing test for tomorrow

--- __tests__/index.js
+++ __tests__/index.js
@@ -4,4 +4,11 @@ describe('date category', () => {
   it('returns today', () => {
     expect(dateCategory(new Date())).toBe('today')
   })
+  it('returns tomorrow', () => {
+
+    const today = new Date()
+    const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1)
+
+    expect(dateCategory(tomorrow)).toBe('tomorrow')
+  })
 })
