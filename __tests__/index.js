c413c2673c84753632a86cd5a91c940262457561
Logan Murphy <lmurphy@learnvest.com>
2018-12-11 13:08:53 -0500

    add broken this week test

--- __tests__/index.js
+++ __tests__/index.js
@@ -7,13 +7,17 @@ describe('date category', () => {
   it('returns tomorrow', () => {
     const today = new Date()
     const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1)
-
     expect(dateCategory(tomorrow)).toBe('tomorrow')
   })
   it('returns yesterday', () => {
     const today = new Date()
     const yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1)
-
     expect(dateCategory(yesterday)).toBe('yesterday')
+  })
+  it('this week', () => {
+    const from = new Date("2018-12-10")
+    expect(dateCategory(new Date(today.getFullYear(), today.getMonth(), today.getDate() + 2))).toBe('this week')
+    expect(dateCategory(new Date(today.getFullYear(), today.getMonth(), today.getDate() + 3))).toBe('this week')
+    expect(dateCategory(new Date(today.getFullYear(), today.getMonth(), today.getDate() + 4))).toBe('this week')
   })
 })
