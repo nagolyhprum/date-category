b20afcac7b28a01bc12b5a2bd3f0f121c05c8f44
Logan Murphy <lmurphy@learnvest.com>
2018-12-11 12:50:45 -0500

    fix tomorrow test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+const ONE_DAY = (1000 * 60 * 60 * 24)
+
 export default (to, from = new Date()) => {
   if(to.getDate() === from.getDate() && to.getMonth() === from.getMonth() && to.getFullYear() === from.getFullYear()) {
     return 'today'
+  }
+  if((to - from) < ONE_DAY) {
+    return 'tomorrow'
   }
 }
