d37c307d6d40b2db007fdff9f3b88cf24e020fc5
Logan Murphy <nagolyhprum@hotmail.com>
2020-09-23 11:18:36 -0400

    add category list, audit package, bump version

--- src/languages/english.js
+++ src/languages/english.js
@@ -1,3 +1,32 @@
 export default {
-  today: 'today'
+  today: 'today',
+  yesterday: 'yesterday',
+  tomorrow: 'tomorrow',
+  before: 'earlier',
+  after: 'later',
+  earlier: {
+    this: {
+      week: 'earlier this week',
+      month: 'earlier this month',
+      year: 'earlier this year'
+    }
+  },
+  this: {
+    weekend: 'this weekend',
+    week: 'this week',
+    month: 'this month',
+    year: 'this year'
+  },
+  next: {
+    weekend: 'next weekend',
+    week: 'next week',
+    month: 'next month',
+    year: 'next year'
+  },
+  last: {
+    weekend: 'last weekend',
+    week: 'last week',
+    month: 'last month',
+    year: 'last year'
+  }
 }
